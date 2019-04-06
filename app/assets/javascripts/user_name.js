3b1751d298ec38be2bd6884ebafe2a13662e2201
YUICHIROONUKI <yuichiro.o0209@gmail.com>
2019-04-06 20:55:32 +0900

    add_new_search_function
    グループ作成画面においてユーザー検索、追加、除去機能を非同期通信で実装した

---
+++ app/assets/javascripts/user_name.js
@@ -0,0 +1,66 @@
+$(document).on('turbolinks:load',function(){
+	function buildHTML(user) {
+	var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.user_name}</p>
+                  <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.user_id}" data-user-name="${user.user_name}">追加</a>
+                </div>`
+                return html;
+            }
+    function appendUser(userId, userName) {
+	var html = `<div class='chat-group-user clearfix js-chat-member' id='${userId}'>
+                  <input name='group[user_ids][]' type='hidden' value="${userId}"">
+                  <p class='chat-group-user__name'>${userName}</p>
+                  <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+                </div>`
+                return html
+            }
+
+	$('#user-search-field').on('keyup', function(){
+		var input = $(this).val();
+
+	    $.ajax({
+			type: 'GET',
+			url: '/users',
+			data: { keyword: input },
+			dataType: 'json'
+		})
+		.done(function(users){
+			$('#user-search-result').empty();
+
+			if (users.length !== 0 && input.length !== 0) {
+				users.forEach(function(user) {
+					var html = buildHTML(user);
+					var currentUserId = $('.chat-group-user').attr("id");
+			        $('#user-search-result').append(html);
+			        var appendUserId = $('.user-search-add').attr("data-user-id");
+			        if (currentUserId === appendUserId ) {
+                       $('#user-search-result').empty();
+			        }
+		        });
+			}
+		})
+		.fail(function(){
+			alert('ユーザー検索に失敗しました');
+		});
+	});
+
+	$(document).on('click', ".user-search-add", function(e){
+		e.preventDefault();
+		var userId = $(this).attr("data-user-id");
+		var userName = $(this).attr("data-user-name");
+    	$('#user-search-result').empty();
+    	var html = appendUser(userId, userName);
+    	$('#chat-group-users').append(html);
+    	});
+
+    $(document).on('click', ".user-search-remove", function(e){
+    	e.preventDefault();
+    	$(this).parent().empty();
+    });
+});
+
+
+
+
+
+
