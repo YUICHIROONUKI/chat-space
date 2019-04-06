2c3d563ff2c0f790146625af2cb278de011375f3
YUICHIROONUKI <yuichiro.o0209@gmail.com>
2019-04-06 14:50:43 +0900

    miss
    間違えたブランチでpushしてしまったので修正用

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,14 @@ class UsersController < ApplicationController
 		end
 	end
 
-	def index
+    def index
 		@users = User.where('name LIKE(?)', "#{params[:keyword]}%")
 		respond_to do |format|
 			format.html
 			format.json
 		end
 	end
+
 
 	private
 
