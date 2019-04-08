712704b59e625fa5dd2b3ab052a2f1753b53bd95
YUICHIROONUKI <yuichiro.o0209@gmail.com>
2019-04-08 14:30:26 +0900

    edit_indent

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,94 +1,94 @@
 $(document).on('turbolinks:load', function(){
-  var buildMessageHTML = function(message) {
+	var buildMessageHTML = function(message) {
 		if (message.content && message.image.url) {
-		  var html = `<div class="right-contents__chat__user" data-id="${message.id}">
-						        <div class="right-contents__chat__user__name">
-						  				${message.user_name}
+			var html = `<div class="right-contents__chat__user" data-id="${message.id}">
+										<div class="right-contents__chat__user__name">
+											${message.user_name}
 										</div>
 										<div class="right-contents__chat__user__date">
-						  				${message.created_at}
+											${message.created_at}
 										</div>
 										<div class="right-contents__chat__user__comment">
-						 				  ${message.content}
+											  ${message.content}
 										</div>
 										<div class="right-contents__chat__user__image">
-						  				<img src="${message.image.url}" class="lower-message__image" >
+											<img src="${message.image.url}" class="lower-message__image" >
 										</div>
-				      		</div>`
+									</div>`
 		} else if (message.content) {
-		  var html = `<div class="right-contents__chat__user" data-id="${message.id}">
+			var html = `<div class="right-contents__chat__user" data-id="${message.id}">
 										<div class="right-contents__chat__user__name">
-						  				${message.user_name}
+											${message.user_name}
 										</div>
 										<div class="right-contents__chat__user__date">
-										  ${message.created_at}
+											${message.created_at}
 										</div>
 										<div class="right-contents__chat__user__comment">
-										  ${message.content}
+											${message.content}
 										</div>
-								  </div>`
+									</div>`
 		} else if (message.image.url) {
-		  var html = `<div class="right-contents__chat__user" data-id="${message.id}">
+			var html = `<div class="right-contents__chat__user" data-id="${message.id}">
 										<div class="right-contents__chat__user__name">
-										  ${message.user_name}
+											${message.user_name}
 										</div>
 										<div class="right-contents__chat__user__date">
-										  ${message.created_at}
+											${message.created_at}
 										</div>
 										<div class="right-contents__chat__user__image">
-										  <img src="${message.image.url}" class="lower-message__image" >
+											<img src="${message.image.url}" class="lower-message__image" >
 										</div>
-								  </div>`
+									</div>`
 		};
 		return html;
-  };
+	};
 	$('.new_message').on('submit', function(e){
-	  e.preventDefault();
-      var formData = new FormData(this);
-	  var href = window.location.href;
-	  $.ajax({
-	    type: "POST",
+		e.preventDefault();
+		var formData = new FormData(this);
+		var href = window.location.href;
+		$.ajax({
+			type: "POST",
 			url: href,
 			data: formData,
 			dataType: 'json',
 			processData: false,
 			contentType: false,
-	  })
-	  .done(function(data){
-	    var html = buildMessageHTML(data);
-	    $('.right-contents__chat').append(html);
-	    $('form')[0].reset();
-	    $('.form__submit').removeAttr('disabled');
-	    $('.right-contents__chat').animate({ scrollTop: $('.right-contents__chat')[0].scrollHeight});
-	    return false
-	    })
-	  .fail(function(){
-	    alert('error');
-	  });
+		})
+		.done(function(data){
+			var html = buildMessageHTML(data);
+			$('.right-contents__chat').append(html);
+			$('form')[0].reset();
+			$('.form__submit').removeAttr('disabled');
+			$('.right-contents__chat').animate({ scrollTop: $('.right-contents__chat')[0].scrollHeight});
+			return false
+		})
+		.fail(function(){
+			alert('error');
+		});
 	});
-  function reloadMessages() {
-	  var last_message_id = $('.right-contents__chat__user:last').data('id');
-	  $.ajax({
-      url: "api/messages",
-      type: 'get',
-      data: {id: last_message_id},
-      dataType: 'json'
-	  })
-	  .done(function(messages){
-	    if (messages) {
+	function reloadMessages() {
+		var last_message_id = $('.right-contents__chat__user:last').data('id');
+		$.ajax({
+			url: "api/messages",
+			type: 'get',
+			data: {id: last_message_id},
+			dataType: 'json'
+		})
+		.done(function(messages){
+			if (messages) {
 				$.each(messages, function(i, message){
-		  		var html = buildMessageHTML(message);
-		  		$('.right-contents__chat').append(html);
+					var html = buildMessageHTML(message);
+					$('.right-contents__chat').append(html);
 				});
-	  		$('.right-contents__chat').animate({ scrollTop: $('.right-contents__chat')[0].scrollHeight});
-	  		return false
-	  	}
-	  })
-	  .fail(function(){
-	    alert('error');
-	  });
+				$('.right-contents__chat').animate({ scrollTop: $('.right-contents__chat')[0].scrollHeight});
+				return false
+			}
+		})
+		.fail(function(){
+			alert('error');
+		});
 	};
-  setInterval(reloadMessages, 5000);
+	setInterval(reloadMessages, 5000);
 });
 
 
