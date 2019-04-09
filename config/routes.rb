0a7db19614de2ccbbc7c986bf0772a741ef56540
YUICHIROONUKI <yuichiro.o0209@gmail.com>
2019-04-09 12:33:06 +0900

    Edit_indents
    インデントを全て修正しました

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,9 @@ Rails.application.routes.draw do
   root 'groups#index'
   resources :users, only: [:edit, :update, :index]
   resources :groups, only: [:create, :edit, :update, :new] do
-	resources :messages, only: [:create, :index]
-	namespace :api do
-		resources :messages, only: :index
-	end
+  resources :messages, only: [:create, :index]
+    namespace :api do
+      resources :messages, only: :index
+    end
   end
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
