95ab130a3ccef905fed34a8882a8b71147bb4022
YUICHIROONUKI <yuichiro.o0209@gmail.com>
2019-04-09 12:42:56 +0900

    edit_indent
    インデント修正しました

--- app/assets/javascripts/user_name.js
+++ app/assets/javascripts/user_name.js
@@ -1,19 +1,19 @@
 $(document).on('turbolinks:load',function() {
   function usersList(user) {
-  var html = `<div class="chat-group-user clearfix">
-                <p class="chat-group-user__name">${user.user_name}</p>
-                <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.user_id}" data-user-name="${user.user_name}">追加</div>
-              </div>`
-              return html;
-      }
+    var html = `<div class="chat-group-user clearfix">
+                  <p class="chat-group-user__name">${user.user_name}</p>
+                  <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.user_id}" data-user-name="${user.user_name}">追加</div>
+                </div>`
+    return html;
+  }
   function appendUser(userId, userName) {
-  var html = `<div class='chat-group-user clearfix js-chat-member' id='${userId}'>
-                <input name='group[user_ids][]' type='hidden' value="${userId}"">
-                <p class='chat-group-user__name'>${userName}</p>
-                <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
-              </div>`
-              return html
-      }
+    var html = `<div class='chat-group-user clearfix js-chat-member' id='${userId}'>
+                  <input name='group[user_ids][]' type='hidden' value="${userId}"">
+                  <p class='chat-group-user__name'>${userName}</p>
+                  <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
+                </div>`
+    return html
+  }
 
   $('#user-search-field').on('keyup', function(){
     var input = $(this).val();
